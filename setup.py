b98d050f0b31134ba34b8f71e5caf89796a1a90d
Hiroyuki Sogo <hsogo12600@gmail.com>
2018-09-05 21:38:19 +0900

    NEW: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name = "memelib",
+    version = "0.0.1",
+    packages=['memelib'],
+)
+
+package_data = {
+    'memelib': ['MEMELib_Academic.dll],
+
+}
